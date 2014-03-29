52d456b9d4b3b2678e31f877d877e8fcbb971401
jingan1009 <jingan1009@gmail.com>
2014-03-28 19:52:27 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     var CityArray = new Array();
     var CityNumber = -1;
@@ -458,7 +457,7 @@ $(document).ready(function(){
         iDiv.className = "block";
         document.getElementsByTagName('body')[0].appendChild(iDiv);
 
-		var m = [80, 80, 80, 80]; // margins
+		var m = [80, 100, 80, 100]; // margins
 		var w = 1000 - m[1] - m[3]; // width
 		var h = 400 - m[0] - m[2]; // height
 		var colors=["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2, #7f7f7f, #c7c7c7, #bcbd22, #dbdb8d, #17becf, #9edae5"];
